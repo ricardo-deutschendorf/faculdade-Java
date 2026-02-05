0fcf203e0d7f7f13fb66c39401fe7d8c85500c64
Ricardo Deutschendorf <163420542+ricardo-deutschendorf@users.noreply.github.com>
2026-02-05 10:16:24 -0300

    testes java

--- Aula1/src/Main.java
+++ Aula1/src/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Selecione seu operador aritimético \n" +
-                "1. para somar \n" +
-                "2. para diminuir \n" +
-                "3. para multiplicar \n" +
-                "4. para dividir \n");
+        System.out.println(
+                "Selecione seu operador aritmético\n" +
+                        "1. para somar\n" +
+                        "2. para diminuir\n" +
+                        "3. para multiplicar\n" +
+                        "4. para dividir\n"
+        );
+
         int operador = scanner.nextInt();
 
         if (operador == 1) {
@@ -18,38 +22,47 @@ public class Main {
             System.out.print("Digite o segundo número: ");
             int num2 = scanner.nextInt();
 
-            int resultado = num1 + num2;
-            System.out.println("Resultado da soma: " + resultado);
-        }
-        if (operador == 2) {
+            System.out.println("Resultado da soma: " + (num1 + num2));
+
+        } else if (operador == 2) {
             System.out.print("Digite o primeiro número: ");
             int num1 = scanner.nextInt();
 
             System.out.print("Digite o segundo número: ");
             int num2 = scanner.nextInt();
 
-            int resultado = num1 - num2;
-            System.out.println("Resultado da subtração: " + resultado);
-        }
-        if (operador == 3) {
+            System.out.println("Resultado da subtração: " + (num1 - num2));
+
+        } else if (operador == 3) {
             System.out.print("Digite o primeiro número: ");
             int num1 = scanner.nextInt();
 
             System.out.print("Digite o segundo número: ");
             int num2 = scanner.nextInt();
 
-            int resultado = num1 * num2;
-            System.out.println("Resultado da multiplicação: " + resultado);
-        }
-        if (operador == 4) {
+            System.out.println("Resultado da multiplicação: " + (num1 * num2));
+
+        } else if (operador == 4) {
             System.out.print("Digite o primeiro número: ");
             int num1 = scanner.nextInt();
 
             System.out.print("Digite o segundo número: ");
             int num2 = scanner.nextInt();
 
-            int resultado = num1 / num2;
-            System.out.println("Resultado da divisão: " + resultado);
+            System.out.println("Resultado da divisão: " + (num1 / num2));
+
+        } else {
+            System.out.println("Você não selecionou uma opção válida");
         }
+
+        System.out.println("Digite um número para mostrar os próximos 5:");
+        int num3 = scanner.nextInt();
+
+        for (int i = num3 + 1; i <= num3 + 5; i++) {
+            System.out.println(i);
+        }
+
+
+        scanner.close();
     }
 }
